17cb3a0dbd80f6999256b35c050975f281be7846
Matheus <90228238+ofmxtheuuz@users.noreply.github.com>
2022-10-20 21:02:01 -0300

    Add files via upload

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,11 @@
+﻿int agora = 1;
+int penultimo = 0;
+
+Console.WriteLine(agora);
+for (int i = 1; i <= 20; i++)
+{
+    var next = agora + penultimo;
+    Console.WriteLine(next);
+    penultimo = agora;
+    agora = next;
+}
